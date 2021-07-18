d46fa6172dbd99fe56c09c760b6353a19033cfa6
jinsilna <zxcv392766!>
2021-07-18 21:40:16 +0900

    Initial Commit

---
+++ TestProject1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestProject1
+//
+//  Created by truemac on 2021/07/18.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
